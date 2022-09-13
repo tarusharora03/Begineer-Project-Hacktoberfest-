b8808f54138d259e5d7f19b905c1c0974a93e41f
tarusharora03 <58811191+tarusharora03@users.noreply.github.com>
2022-09-13 12:10:59 +0530

    Add files via upload

---
+++ float_add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    float a,b,c;
+    printf("Enter the value for A: \n");
+    scanf("%f", &a);
+    printf("Enter the value for B: \n");
+    scanf("%f", &b);
+    c=a+b;
+    printf("The Value of A+B: %f", c);
+    return 0;
+}
---
+++ int_add.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+    int a,b,c;
+    printf("Enter the value for A: \n");
+    scanf("%d", &a);
+    printf("Enter the value for B: \n");
+    scanf("%d", &b);
+    c=a+b;
+    printf("The Value of A+B: %d", c);
+    return 0;
+}
